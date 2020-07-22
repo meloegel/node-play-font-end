b3773b62c6ea7e5588a9f84808c597804dc53f1d
meloegel <meloegel@gmail.com>
2020-07-22 19:21:31 -0400

    updates

--- node-play/src/utils/axiosWithAuth.js
+++ node-play/src/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ export const axiosWithAuth = () => {
     const token = localStorage.getItem("token");
     return axios.create({
         headers: {
-            authorization: token,
+            Authorization: token,
         },
         baseURL: "https://cors-anywhere.herokuapp.com/https://node-play.herokuapp.com/",
     });
